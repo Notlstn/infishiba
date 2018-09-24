b20f39ea8daf9d706865cb4a101eaa414996deef
Jędrzej Skrzypczak <j.skrzypczak@protonmail.com>
2018-09-24 06:38:48 +0200

    Push

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { BrowserRouter } from "react-router-dom";
 const store = createStore(reducer);
 
 const app = (
-    <BrowserRouter basename="https://notlstn.github.io/infishiba/">
+    <BrowserRouter>
         <App />
     </BrowserRouter>
 );
